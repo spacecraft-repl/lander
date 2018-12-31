81e06125af98d61f4556c17f560d2afb88c92b68
Julius Zerwick <jrzerwick@gmail.com>
2018-12-31 11:31:51 -0600

    Added class methods to show linked lists and trees

--- lib/lander.rb
+++ lib/lander.rb
@@ -9,10 +9,22 @@ require_relative 'lander/array_with_pointers'
 #   # Your code goes here...
 # end
 
-class Lander
+class LanderClass
 	def self.show_array(arr, left_pointer, right_pointer)
 		ShowArray(arr, left_pointer, right_pointer)
+	end
+
+	def self.show_linked_list(arr)
+		puts createLinkedList(arr)
+	end
+
+	def self.show_tree(arr)
+		displayTree(arr)
 	end
 end
 
 LanderClass.show_array([1,2,3,4], 2, 3)
+puts ""
+LanderClass.show_linked_list([1,2,3,4])
+puts ""
+LanderClass.show_tree([1,2,3,4,5])
