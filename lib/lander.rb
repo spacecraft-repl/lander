4fc5866d5579e905cd8a322392c302a23995ad83
Julius Zerwick <jrzerwick@gmail.com>
2018-12-31 11:40:44 -0600

    Fixed TypeError bug

--- lib/lander.rb
+++ lib/lander.rb
@@ -2,14 +2,13 @@ require_relative 'lander/version'
 require_relative 'lander/list_node'
 require_relative 'lander/tree_node'
 require_relative 'lander/array_with_pointers'
-# require 'lander/heaps'
 
 # module Lander
 #   class Error < StandardError; end
 #   # Your code goes here...
 # end
 
-class LanderClass
+module Lander
 	def self.show_array(arr, left_pointer, right_pointer)
 		ShowArray(arr, left_pointer, right_pointer)
 	end
@@ -23,8 +22,8 @@ class LanderClass
 	end
 end
 
-LanderClass.show_array([1,2,3,4], 2, 3)
-puts ""
-LanderClass.show_linked_list([1,2,3,4])
-puts ""
-LanderClass.show_tree([1,2,3,4,5])
+# Lander.show_array([1,2,3,4], 2, 3)
+# puts ""
+# Lander.show_linked_list([1,2,3,4])
+# puts ""
+# Lander.show_tree([1,2,3,4,5])
