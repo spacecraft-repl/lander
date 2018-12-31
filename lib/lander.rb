5b996f1f4ddbccf2a1cfb701ae9e11c15ad3d35b
Julius Zerwick <jrzerwick@gmail.com>
2018-12-31 10:25:23 -0600

    Added show_array class method to Lander class

--- lib/lander.rb
+++ lib/lander.rb
@@ -4,7 +4,15 @@ require_relative 'lander/tree_node'
 require_relative 'lander/array_with_pointers'
 # require 'lander/heaps'
 
-module Lander
-  class Error < StandardError; end
-  # Your code goes here...
+# module Lander
+#   class Error < StandardError; end
+#   # Your code goes here...
+# end
+
+class Lander
+	def self.show_array(arr, left_pointer, right_pointer)
+		ShowArray(arr, left_pointer, right_pointer)
+	end
 end
+
+LanderClass.show_array([1,2,3,4], 2, 3)
